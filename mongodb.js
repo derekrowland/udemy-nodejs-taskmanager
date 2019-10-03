42b84f78daeca535ca86bea692af48fedfa285fc
Rowland, Derek <derek.rowland@garmin.com>
2019-10-03 14:47:12 -0500

    mongo connect, insert one and many

--- mongodb.js
+++ mongodb.js
@@ -6,11 +6,47 @@ const mongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const database = 'task-manager'
 
-mongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database!')
-    } else {
-        console.log('Connected to MongoDb: ' + connectionURL)
-    }
+mongoClient.connect(connectionURL, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }, (error, client) => {
     
-})
+        if (error) {
+            return console.log('Unable to connect to database!')
+        } 
+        
+        const db = client.db(database)
+        
+        // db.collection('users').insertOne({
+        //     name: 'Derek',
+        //     age: 31
+        //     }, (error, result) => {
+        //     if (error) {
+        //         return console.log('Unable to insert user')
+        //     } 
+
+        //     console.log(result.ops)
+        // })
+
+        db.collection('users').insertMany(
+            [
+                {
+                    name: 'Jen',
+                    age: 32
+                },
+                {
+                    name: 'Jerry',
+                    age: 40
+                }
+            ], (error, result) => {
+                if (error) {
+                    return console.log('Unable to insert documents')
+                }
+
+                console.log(result.ops)
+            }
+        )
+    }
+)
+
