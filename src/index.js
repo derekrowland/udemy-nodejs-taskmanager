785221259e22582c4e4047c522e1184a45f6819a
Rowland, Derek <derek.rowland@garmin.com>
2019-10-08 15:26:39 -0500

    challenge, tasks and users routes

--- src/index.js
+++ src/index.js
@@ -23,12 +23,54 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    User.findById(id).then((user) => {
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+// Task paths
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
         res.send(task)
     }).catch((e) => {
-        res.status(400).send(e)
+        res.status(500).send(e)
+    })
+})
+
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id
+    Task.findById(id).then((task) => {
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
