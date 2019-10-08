bbedbf673fef5a25a5c21175739896b0eaf84686
Rowland, Derek <derek.rowland@garmin.com>
2019-10-08 16:45:01 -0500

    refactored index.js with async

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,16 +1,6 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-// //5d9ceddba4e85e709c2eaa11
-// User.findByIdAndUpdate('5d9ceddba4e85e709c2eaa11', {age:10}).then((user) => {
-//     console.log(user)
-//     return User.countDocuments({age:10})
-// }).then((result) => {
-//     console.log(result)
-// }).catch(e => {
-//     console.log(e)
-// })
-
 const updateAgeAndCount = async (id, age) => {
     const user = await User.findByIdAndUpdate(id, { age })
     const count = await User.countDocuments({age})
