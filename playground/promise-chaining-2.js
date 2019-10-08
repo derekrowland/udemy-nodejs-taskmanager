25b953e041df67cacc0d0b9f0c6e7f94b728f512
Rowland, Derek <derek.rowland@garmin.com>
2019-10-08 16:27:04 -0500

    chaining challenge

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,11 +2,23 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 //5d9caea3a9c0fd7418124cfb
-Task.findByIdAndDelete('5d9caea3a9c0fd7418124cfb').then((user)=> {
-    console.log(user)
-    return Task.countDocuments({completed:false})
-}).then(result => {
-    console.log(result)
+// Task.findByIdAndDelete('5d9caea3a9c0fd7418124cfb').then((user)=> {
+//     console.log(user)
+//     return Task.countDocuments({completed:false})
+// }).then(result => {
+//     console.log(result)
+// }).catch(e => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed:false})
+    return count
+}
+
+deleteTaskAndCount('5d9cebd90bd42c8478b58f3d').then(count => {
+    console.log(count)
 }).catch(e => {
-    console.log(e)
+    console.log('e', e)
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,12 +1,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-//5d9ceddba4e85e709c2eaa11
-User.findByIdAndUpdate('5d9ceddba4e85e709c2eaa11', {age:10}).then((user) => {
-    console.log(user)
-    return User.countDocuments({age:10})
-}).then((result) => {
-    console.log(result)
+// //5d9ceddba4e85e709c2eaa11
+// User.findByIdAndUpdate('5d9ceddba4e85e709c2eaa11', {age:10}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age:10})
+// }).then((result) => {
+//     console.log(result)
+// }).catch(e => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount('5d9ceddba4e85e709c2eaa11', 10).then(count => {
+    console.log(count)
 }).catch(e => {
-    console.log(e)
+    console.log('e', e)
 })
